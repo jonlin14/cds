723bdb9878b999a152eb5f1b148e286d40fef098
Jonathan Lin & Conor Baumgart <student@epicodus.com>
2015-03-05 16:04:36 -0800

    Cd organizer can now recieve CDS and show you the list

--- views/front.php
+++ views/front.php
@@ -4,6 +4,21 @@
     </head>
     <body>
         <h1>Tell me which CDs you have: </h1>
+        <form action="/create_cd" method='post'>
+            <label for='name'>CD name: </label>
+            <input id='name' name='name' type='text'>
 
+            <button type='submit'>Enter!</button>
+        </form>
+
+        <h1> Here are your CDs </h1>
+        {% if cds is not empty %}
+                <p>Here are all your CDs:</p>
+                <ul>
+                    {% for cd in cds %}
+                        <li>{{ cd.getAlbum }}</li>
+                    {% endfor %}
+                </ul>
+            {% endif %}
     </body>
 </html>
